097e7b415c3a4b9689231c37cffd8e091f945ad0
Juan Pablo Bernárdez Lis <jpulus@hotmail.com>
2023-08-10 12:58:31 +0200

    Configured tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(() => {
+    return {
+        plugins: [react()],
+
+        test: {
+            globals: true,
+            environment: 'jsdom',
+            setupFiles: './src/setupTest.ts',
+            coverage: {
+                provider: 'c8',
+                reporter: ['text', 'json', 'html', 'lcov'],
+            },
+        },
+    };
+});

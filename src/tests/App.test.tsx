35badfc6ac7397c4d34a836814ed1eec06440d3e
Juan Pablo Bernárdez Lis <jpulus@hotmail.com>
2023-08-14 09:13:53 +0200

    mend

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,12 +1,10 @@
 import { render } from '@testing-library/react';
 
-// El componente Dummy solo renderiza un mensaje en este ejemplo
 const Dummy = () => {
     return <div>Hello, world!</div>;
 };
 
 test('renders dummy component', () => {
-    // Renderiza el componente Dummy
     const { getByText } = render(<Dummy />);
 
     // Verifica que el mensaje se muestre en la pantalla
